85813155f4dcfee096b92c747dae53b592fef93c
Sho Hashimoto <hashimoto@shokai.org>
2014-04-08 19:37:04 +0900

    show error messages

--- irkit-2ch-tv.rb
+++ irkit-2ch-tv.rb
@@ -7,7 +7,15 @@ page = Nokogiri.parse HTTParty.get("http://bc2ch.net/").body
 tr = page.xpath('//table[@class="sitetotal"]//td[@class="station"]')
 channels = tr.map{|i| i.text }
 p channels
+ch = channels.first
 
-irkit = IRKit::Device.find.first
-ir_data = IRKit::Data["IR"][channels.first]
+unless irkit = IRKit::Device.find.first
+  puts "IRKit not found"
+end
+
+unless ir_data = IRKit::App::Data["IR"][ch]
+  puts %Q{IR-Data "#{ch}" not found}
+  exit 1
+end
+
 irkit.post_messages ir_data
